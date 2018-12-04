b229fe3d5c322acf25d4967a92e75dd4b1bae6ed
Kevin Herrera <kevingato05@gmail.com>
2018-12-03 18:02:43 -0600

    Agregue clases de "Aviones" y "CategoriasVuelos"

---
+++ Sistema Aeropuerto/Aeropuerto_DAL/Cat_Man/Cls_CategoriasVuelos_DAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aeropuerto_DAL.Cat_Man
+{
+   public  class Cls_CategoriasVuelos_DAL
+    {
+    }
+}
