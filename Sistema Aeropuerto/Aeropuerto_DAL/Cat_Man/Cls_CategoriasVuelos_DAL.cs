afa4fd130f093764b8727f7b6034410929477c0e
kevin herrera b <kevingato05@gmail.com>
2018-12-04 23:12:13 -0600

    agregar datos a las clases de Aviones y CategoriasVuelos. (DAL y BLL)

--- Sistema Aeropuerto/Aeropuerto_DAL/Cat_Man/Cls_CategoriasVuelos_DAL.cs
+++ Sistema Aeropuerto/Aeropuerto_DAL/Cat_Man/Cls_CategoriasVuelos_DAL.cs
@@ -3,10 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace Aeropuerto_DAL.Cat_Man
 {
    public  class Cls_CategoriasVuelos_DAL
     {
+        public DataTable OBJ_DataTable = new DataTable();
+
+        private char _cIdCategoria, _cAccion, _cIdEstado;
+        private string _sError, _sDescCategoria;
+
+        public char CAccion
+        {
+            get
+            {
+                return _cAccion;
+            }
+
+            set
+            {
+                _cAccion = value;
+            }
+        }
+
+        public char CIdCategoria
+        {
+            get
+            {
+                return _cIdCategoria;
+            }
+
+            set
+            {
+                _cIdCategoria = value;
+            }
+        }
+
+        public char CIdEstado
+        {
+            get
+            {
+                return _cIdEstado;
+            }
+
+            set
+            {
+                _cIdEstado = value;
+            }
+        }
+
+        public string SDescCategoria
+        {
+            get
+            {
+                return _sDescCategoria;
+            }
+
+            set
+            {
+                _sDescCategoria = value;
+            }
+        }
+
+        public string SError
+        {
+            get
+            {
+                return _sError;
+            }
+
+            set
+            {
+                _sError = value;
+            }
+        }
     }
 }
