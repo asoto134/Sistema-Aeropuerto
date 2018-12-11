b8140c3967cfd03af36b8b34cb07e22c57024a1b
RoderickVonQuack <44595975+RoderickVonQuack@users.noreply.github.com>
2018-12-10 20:03:27 -0600

    Changes in Past Versions of FRM's Scheiße

--- Sistema Aeropuerto/Aeropuerto_Munich_UI/Pantallas_Terciarias/FRM_Editar_Tipos_Aviones.Designer.cs
+++ Sistema Aeropuerto/Aeropuerto_Munich_UI/Pantallas_Terciarias/FRM_Editar_Tipos_Aviones.Designer.cs
@@ -28,20 +28,259 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRM_Editar_Tipos_Aviones));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.btn_Guardar = new System.Windows.Forms.Button();
+            this.grp_Informacion = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_DescrAvion = new System.Windows.Forms.TextBox();
+            this.cmb_Estados = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_ID = new System.Windows.Forms.TextBox();
+            this.txt_TipoAvion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_CantidadPasa = new System.Windows.Forms.TextBox();
+            this.txt_CantidadPeso = new System.Windows.Forms.TextBox();
+            this.groupBox2.SuspendLayout();
+            this.grp_Informacion.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.btn_Salir);
+            this.groupBox2.Controls.Add(this.btn_Guardar);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(10, 281);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(456, 105);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controles";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(353, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Cancelar";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(138, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Guardar";
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Salir.BackgroundImage = global::Aeropuerto_Munich_UI.Properties.Resources.img_btn_Cancelar;
+            this.btn_Salir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Salir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Salir.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_Salir.Location = new System.Drawing.Point(295, 28);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(52, 45);
+            this.btn_Salir.TabIndex = 4;
+            this.btn_Salir.UseVisualStyleBackColor = false;
+            // 
+            // btn_Guardar
+            // 
+            this.btn_Guardar.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Guardar.BackgroundImage = global::Aeropuerto_Munich_UI.Properties.Resources.img_btn_Guardar;
+            this.btn_Guardar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Guardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Guardar.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_Guardar.Location = new System.Drawing.Point(76, 32);
+            this.btn_Guardar.Name = "btn_Guardar";
+            this.btn_Guardar.Size = new System.Drawing.Size(47, 41);
+            this.btn_Guardar.TabIndex = 5;
+            this.btn_Guardar.UseVisualStyleBackColor = false;
+            // 
+            // grp_Informacion
+            // 
+            this.grp_Informacion.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.grp_Informacion.Controls.Add(this.txt_CantidadPeso);
+            this.grp_Informacion.Controls.Add(this.txt_CantidadPasa);
+            this.grp_Informacion.Controls.Add(this.label8);
+            this.grp_Informacion.Controls.Add(this.label7);
+            this.grp_Informacion.Controls.Add(this.label4);
+            this.grp_Informacion.Controls.Add(this.txt_DescrAvion);
+            this.grp_Informacion.Controls.Add(this.cmb_Estados);
+            this.grp_Informacion.Controls.Add(this.label3);
+            this.grp_Informacion.Controls.Add(this.txt_ID);
+            this.grp_Informacion.Controls.Add(this.txt_TipoAvion);
+            this.grp_Informacion.Controls.Add(this.label1);
+            this.grp_Informacion.Controls.Add(this.label2);
+            this.grp_Informacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grp_Informacion.Location = new System.Drawing.Point(11, 10);
+            this.grp_Informacion.Name = "grp_Informacion";
+            this.grp_Informacion.Size = new System.Drawing.Size(455, 263);
+            this.grp_Informacion.TabIndex = 12;
+            this.grp_Informacion.TabStop = false;
+            this.grp_Informacion.Text = "Datos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tipo de Avión:";
+            // 
+            // txt_DescrAvion
+            // 
+            this.txt_DescrAvion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txt_DescrAvion.Location = new System.Drawing.Point(198, 102);
+            this.txt_DescrAvion.MaxLength = 90;
+            this.txt_DescrAvion.Name = "txt_DescrAvion";
+            this.txt_DescrAvion.Size = new System.Drawing.Size(249, 21);
+            this.txt_DescrAvion.TabIndex = 6;
+            // 
+            // cmb_Estados
+            // 
+            this.cmb_Estados.FormattingEnabled = true;
+            this.cmb_Estados.Location = new System.Drawing.Point(198, 226);
+            this.cmb_Estados.Name = "cmb_Estados";
+            this.cmb_Estados.Size = new System.Drawing.Size(249, 24);
+            this.cmb_Estados.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Estado:";
+            // 
+            // txt_ID
+            // 
+            this.txt_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txt_ID.Location = new System.Drawing.Point(198, 21);
+            this.txt_ID.MaxLength = 1;
+            this.txt_ID.Name = "txt_ID";
+            this.txt_ID.Size = new System.Drawing.Size(100, 21);
+            this.txt_ID.TabIndex = 2;
+            // 
+            // txt_TipoAvion
+            // 
+            this.txt_TipoAvion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txt_TipoAvion.Location = new System.Drawing.Point(198, 61);
+            this.txt_TipoAvion.MaxLength = 90;
+            this.txt_TipoAvion.Name = "txt_TipoAvion";
+            this.txt_TipoAvion.Size = new System.Drawing.Size(249, 21);
+            this.txt_TipoAvion.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Tipo de Avión:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Descripción:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(45, 187);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(150, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Capacidad de Peso:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 145);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(185, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Capacidad de Pasajeros:";
+            // 
+            // txt_CantidadPasa
+            // 
+            this.txt_CantidadPasa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txt_CantidadPasa.Location = new System.Drawing.Point(198, 143);
+            this.txt_CantidadPasa.MaxLength = 90;
+            this.txt_CantidadPasa.Name = "txt_CantidadPasa";
+            this.txt_CantidadPasa.Size = new System.Drawing.Size(249, 21);
+            this.txt_CantidadPasa.TabIndex = 10;
+            // 
+            // txt_CantidadPeso
+            // 
+            this.txt_CantidadPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txt_CantidadPeso.Location = new System.Drawing.Point(198, 185);
+            this.txt_CantidadPeso.MaxLength = 90;
+            this.txt_CantidadPeso.Name = "txt_CantidadPeso";
+            this.txt_CantidadPeso.Size = new System.Drawing.Size(249, 21);
+            this.txt_CantidadPeso.TabIndex = 11;
             // 
             // FRM_Editar_Tipos_Aviones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(421, 285);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(478, 398);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.grp_Informacion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FRM_Editar_Tipos_Aviones";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FRM_Editar_Tipos_Aviones";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.grp_Informacion.ResumeLayout(false);
+            this.grp_Informacion.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_Salir;
+        private System.Windows.Forms.Button btn_Guardar;
+        private System.Windows.Forms.GroupBox grp_Informacion;
+        private System.Windows.Forms.TextBox txt_CantidadPeso;
+        private System.Windows.Forms.TextBox txt_CantidadPasa;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_DescrAvion;
+        private System.Windows.Forms.ComboBox cmb_Estados;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_ID;
+        private System.Windows.Forms.TextBox txt_TipoAvion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
