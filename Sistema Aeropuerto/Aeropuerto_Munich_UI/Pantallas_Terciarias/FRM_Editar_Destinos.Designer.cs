07053348ed49df1c7e70b359b8da0bb8d83d5875
Josua22 <44511594+Josua22@users.noreply.github.com>
2018-12-15 23:18:45 -0600

    Frm_Editar_Destino

--- Sistema Aeropuerto/Aeropuerto_Munich_UI/Pantallas_Terciarias/FRM_Editar_Destinos.Designer.cs
+++ Sistema Aeropuerto/Aeropuerto_Munich_UI/Pantallas_Terciarias/FRM_Editar_Destinos.Designer.cs
@@ -28,19 +28,232 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.btn_Guardar = new System.Windows.Forms.Button();
+            this.grp_Informacion = new System.Windows.Forms.GroupBox();
+            this.cb_Aerolinea = new System.Windows.Forms.ComboBox();
+            this.cb_Pais_S = new System.Windows.Forms.ComboBox();
+            this.cb_Pais_Ll = new System.Windows.Forms.ComboBox();
+            this.cb_Estado = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_ID_Destino = new System.Windows.Forms.TextBox();
+            this.txt_Nombre_Destino = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.grp_Informacion.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox2.Controls.Add(this.btn_Salir);
+            this.groupBox2.Controls.Add(this.btn_Guardar);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 281);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(408, 88);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controles";
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Salir.BackgroundImage = global::Aeropuerto_Munich_UI.Properties.Resources.img_btn_Cancelar;
+            this.btn_Salir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Salir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Salir.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_Salir.Location = new System.Drawing.Point(276, 32);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(52, 45);
+            this.btn_Salir.TabIndex = 4;
+            this.btn_Salir.UseVisualStyleBackColor = false;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
+            // 
+            // btn_Guardar
+            // 
+            this.btn_Guardar.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Guardar.BackgroundImage = global::Aeropuerto_Munich_UI.Properties.Resources.img_btn_Guardar;
+            this.btn_Guardar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Guardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Guardar.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_Guardar.Location = new System.Drawing.Point(74, 34);
+            this.btn_Guardar.Name = "btn_Guardar";
+            this.btn_Guardar.Size = new System.Drawing.Size(47, 41);
+            this.btn_Guardar.TabIndex = 5;
+            this.btn_Guardar.UseVisualStyleBackColor = false;
+            this.btn_Guardar.Click += new System.EventHandler(this.btn_Guardar_Click);
+            // 
+            // grp_Informacion
+            // 
+            this.grp_Informacion.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.grp_Informacion.Controls.Add(this.cb_Aerolinea);
+            this.grp_Informacion.Controls.Add(this.cb_Pais_S);
+            this.grp_Informacion.Controls.Add(this.cb_Pais_Ll);
+            this.grp_Informacion.Controls.Add(this.cb_Estado);
+            this.grp_Informacion.Controls.Add(this.label6);
+            this.grp_Informacion.Controls.Add(this.label5);
+            this.grp_Informacion.Controls.Add(this.label3);
+            this.grp_Informacion.Controls.Add(this.label4);
+            this.grp_Informacion.Controls.Add(this.txt_ID_Destino);
+            this.grp_Informacion.Controls.Add(this.txt_Nombre_Destino);
+            this.grp_Informacion.Controls.Add(this.label1);
+            this.grp_Informacion.Controls.Add(this.label2);
+            this.grp_Informacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grp_Informacion.Location = new System.Drawing.Point(12, 12);
+            this.grp_Informacion.Name = "grp_Informacion";
+            this.grp_Informacion.Size = new System.Drawing.Size(408, 263);
+            this.grp_Informacion.TabIndex = 16;
+            this.grp_Informacion.TabStop = false;
+            this.grp_Informacion.Text = "Datos";
+            // 
+            // cb_Aerolinea
+            // 
+            this.cb_Aerolinea.FormattingEnabled = true;
+            this.cb_Aerolinea.Location = new System.Drawing.Point(131, 54);
+            this.cb_Aerolinea.Name = "cb_Aerolinea";
+            this.cb_Aerolinea.Size = new System.Drawing.Size(250, 24);
+            this.cb_Aerolinea.TabIndex = 15;
+            // 
+            // cb_Pais_S
+            // 
+            this.cb_Pais_S.FormattingEnabled = true;
+            this.cb_Pais_S.Location = new System.Drawing.Point(131, 124);
+            this.cb_Pais_S.Name = "cb_Pais_S";
+            this.cb_Pais_S.Size = new System.Drawing.Size(250, 24);
+            this.cb_Pais_S.TabIndex = 14;
+            // 
+            // cb_Pais_Ll
+            // 
+            this.cb_Pais_Ll.FormattingEnabled = true;
+            this.cb_Pais_Ll.Location = new System.Drawing.Point(131, 159);
+            this.cb_Pais_Ll.Name = "cb_Pais_Ll";
+            this.cb_Pais_Ll.Size = new System.Drawing.Size(250, 24);
+            this.cb_Pais_Ll.TabIndex = 13;
+            // 
+            // cb_Estado
+            // 
+            this.cb_Estado.FormattingEnabled = true;
+            this.cb_Estado.Location = new System.Drawing.Point(131, 192);
+            this.cb_Estado.Name = "cb_Estado";
+            this.cb_Estado.Size = new System.Drawing.Size(250, 24);
+            this.cb_Estado.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Estado:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ID Aerolinea:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "País Salida:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "País Llegada";
+            // 
+            // txt_ID_Destino
+            // 
+            this.txt_ID_Destino.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txt_ID_Destino.Location = new System.Drawing.Point(131, 24);
+            this.txt_ID_Destino.MaxLength = 1;
+            this.txt_ID_Destino.Name = "txt_ID_Destino";
+            this.txt_ID_Destino.Size = new System.Drawing.Size(250, 21);
+            this.txt_ID_Destino.TabIndex = 2;
+            this.txt_ID_Destino.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_ID_Destino_KeyPress);
+            // 
+            // txt_Nombre_Destino
+            // 
+            this.txt_Nombre_Destino.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txt_Nombre_Destino.Location = new System.Drawing.Point(131, 89);
+            this.txt_Nombre_Destino.MaxLength = 90;
+            this.txt_Nombre_Destino.Name = "txt_Nombre_Destino";
+            this.txt_Nombre_Destino.Size = new System.Drawing.Size(250, 21);
+            this.txt_Nombre_Destino.TabIndex = 3;
+            this.txt_Nombre_Destino.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Nombre_Destino_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Destino:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre Destino:";
             // 
             // FRM_Editar_Destinos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(421, 285);
+            this.ClientSize = new System.Drawing.Size(431, 380);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.grp_Informacion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FRM_Editar_Destinos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FRM_Editar_Destinos";
+            this.Load += new System.EventHandler(this.FRM_Editar_Destinos_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.grp_Informacion.ResumeLayout(false);
+            this.grp_Informacion.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_Salir;
+        private System.Windows.Forms.Button btn_Guardar;
+        private System.Windows.Forms.GroupBox grp_Informacion;
+        private System.Windows.Forms.ComboBox cb_Aerolinea;
+        private System.Windows.Forms.ComboBox cb_Pais_S;
+        private System.Windows.Forms.ComboBox cb_Pais_Ll;
+        private System.Windows.Forms.ComboBox cb_Estado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_ID_Destino;
+        private System.Windows.Forms.TextBox txt_Nombre_Destino;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Sistema Aeropuerto/Aeropuerto_Munich_UI/Pantallas_Terciarias/FRM_Editar_Destinos.cs
+++ Sistema Aeropuerto/Aeropuerto_Munich_UI/Pantallas_Terciarias/FRM_Editar_Destinos.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Aeropuerto_BLL.Cat_Man;
+using Aeropuerto_DAL.Cat_Man;
 
 namespace Aeropuerto_Munich_UI.Pantallas_Terciarias
 {
@@ -15,6 +17,240 @@ namespace Aeropuerto_Munich_UI.Pantallas_Terciarias
         public FRM_Editar_Destinos()
         {
             InitializeComponent();
+        }
+
+        cls_Destinos_BLL Obj_Destinos_BLL = new cls_Destinos_BLL();
+        public cls_Destinos_DAL Obj_Editar_Destino;
+
+        //iNSTANCIAR PARA EL COMBO ESTADO
+        cls_Estados_BLL Obj_Estados_BLL = new cls_Estados_BLL();
+        cls_Estados_DAL Obj_Estados_DAL = new cls_Estados_DAL();
+
+        //INSTANCIAR PARA EL COMBO aEROLINEAS
+        cls_Aerolineas_BLL Obj_Aerolineas_BLL = new cls_Aerolineas_BLL();
+        cls_Aerolineas_DAL Obj_Aerolineas_DAL = new cls_Aerolineas_DAL();
+
+        //INSTANCIAR PARA COMBO PAISES DE DESTINO
+        cls_Paises_BLL Obj_Paises_BLL = new cls_Paises_BLL();
+        cls_Paises_DAL Obj_Paises_DAL = new cls_Paises_DAL();
+
+        //METODO PARA CARGAR COMBOS
+        private void CargarCombosEstado()
+        {
+            Obj_Estados_BLL.Listar(ref Obj_Estados_DAL);
+
+            if (Obj_Estados_DAL.SError == string.Empty)
+            {
+                cb_Estado.DataSource = null;
+                cb_Estado.DataSource = Obj_Estados_DAL.OBJ_DataTable;
+
+                Obj_Estados_DAL.OBJ_DataTable.Rows.Add("-", "-- SELECCIONAR UN ESTADO --");
+
+                cb_Estado.DisplayMember = "Descripcion";
+                cb_Estado.ValueMember = "IdEstado";
+            }
+            else
+            {
+                MessageBox.Show("SE PRESENTO UN ERROR A LA HORA DE CARGAR EL COMBO DE ESTADOS.\n\nERROR: [ " + Obj_Estados_DAL.SError + " ].",
+                                    "ERROR",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void CargarCombosAerolineas()
+        {
+            Obj_Aerolineas_BLL.Listar(ref Obj_Aerolineas_DAL);
+
+            if (Obj_Aerolineas_DAL.SError == string.Empty)
+            {
+                cb_Aerolinea.DataSource = null;
+                cb_Aerolinea.DataSource = Obj_Aerolineas_DAL.OBJ_DataTable;
+
+                Obj_Aerolineas_DAL.OBJ_DataTable.Rows.Add("0", "-- SELECCIONAR UNA AEROLINEA --");
+
+                cb_Aerolinea.DisplayMember = "NombreAerolinea";
+                cb_Aerolinea.ValueMember = "IdAerolinea";
+            }
+            else
+            {
+                MessageBox.Show("SE PRESENTO UN ERROR A LA HORA DE CARGAR EL COMBO DE AEROLINEA.\n\nERROR: [ " + Obj_Aerolineas_DAL.SError + " ].",
+                                    "ERROR",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void CargarCombosPaises()
+        {
+            Obj_Paises_BLL.Listar(ref Obj_Paises_DAL);
+
+            if (Obj_Paises_DAL.SError == string.Empty)
+            {
+
+                cb_Pais_S.DataSource = null;
+                cb_Pais_S.DataSource = Obj_Paises_DAL.OBJ_DataTable;
+
+                Obj_Paises_DAL.OBJ_DataTable.Rows.Add("0", "-- SELECCIONAR UN PAIS --");
+
+                cb_Pais_S.DisplayMember = "Nombre País";
+                cb_Pais_S.ValueMember = "IdPais";
+
+            }
+            else
+            {
+                MessageBox.Show("SE PRESENTO UN ERROR A LA HORA DE CARGAR EL COMBO DE AEROLINEA.\n\nERROR: [ " + Obj_Paises_DAL.SError + " ].",
+                                    "ERROR",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void CargarCombosPaises2()
+        {
+            Obj_Paises_BLL.Listar(ref Obj_Paises_DAL);
+
+            if (Obj_Paises_DAL.SError == string.Empty)
+            {
+                cb_Pais_Ll.DataSource = null;
+                cb_Pais_Ll.DataSource = Obj_Paises_DAL.OBJ_DataTable;
+
+                Obj_Paises_DAL.OBJ_DataTable.Rows.Add("0", "-- SELECCIONAR UN PAIS --");
+
+                cb_Pais_Ll.DisplayMember = "Nombre País";
+                cb_Pais_Ll.ValueMember = "IdPais";
+            }
+            else
+            {
+                MessageBox.Show("SE PRESENTO UN ERROR A LA HORA DE CARGAR EL COMBO DE AEROLINEA.\n\nERROR: [ " + Obj_Paises_DAL.SError + " ].",
+                                    "ERROR",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+            }
+        }
+
+        //METODO PARA CARGAR DATOS
+        private void CargarDatos()
+        {
+            CargarCombosAerolineas();
+            CargarCombosEstado();
+            CargarCombosPaises();
+            CargarCombosPaises2();
+
+            if (Obj_Editar_Destino.cAccion == 'I')
+            {
+                txt_ID_Destino.Enabled = true;
+                txt_ID_Destino.Text = string.Empty;
+                cb_Aerolinea.SelectedValue = "0";
+                txt_Nombre_Destino.Text = string.Empty;
+                cb_Pais_S.SelectedValue = "0";
+                cb_Pais_Ll.SelectedValue = "0";
+                cb_Estado.SelectedValue = "-";
+                grp_Informacion.Text = "Insertar Datos de Destino";
+            }
+            else
+            {
+                txt_ID_Destino.Enabled = false;
+                txt_ID_Destino.Text = Obj_Editar_Destino.cIdDestino.ToString();
+                cb_Aerolinea.SelectedValue = Obj_Editar_Destino.cIdAerolinea.ToString();
+                txt_Nombre_Destino.Text = Obj_Editar_Destino.sNombreDestino.ToString();
+                cb_Pais_S.SelectedValue = Obj_Editar_Destino.cPaisSalida.ToString();
+                cb_Pais_Ll.SelectedValue = Obj_Editar_Destino.cPaisEntrada;
+                cb_Estado.SelectedValue = Obj_Editar_Destino.cIdEstado.ToString();
+                grp_Informacion.Text = "Modifiecar Datos de Destino";
+            }
+        }
+
+        private void btn_Guardar_Click(object sender, EventArgs e)
+        {
+            if (cb_Aerolinea.SelectedValue.ToString() == "0" || cb_Pais_S.SelectedValue.ToString() == "0"
+               || cb_Pais_Ll.SelectedValue.ToString() == "0" || cb_Estado.SelectedValue.ToString() == "-")
+            {
+                MessageBox.Show("DEBE SELECCIONAR ESTADO, AEROLINEA, PAIS SALIDA Y LLEGADA PARA GUARDAR LA INFORMACIÓN",
+                                "ALERTA",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                Obj_Destinos_BLL = new cls_Destinos_BLL();
+
+                Obj_Editar_Destino.cIdDestino = Convert.ToChar(txt_ID_Destino.Text.ToString());
+                Obj_Editar_Destino.cIdAerolinea = Convert.ToInt32(cb_Aerolinea.SelectedValue.ToString());
+                Obj_Editar_Destino.sNombreDestino = txt_Nombre_Destino.Text;
+                Obj_Editar_Destino.cPaisSalida = Convert.ToChar(cb_Pais_S.SelectedValue.ToString());
+                Obj_Editar_Destino.cPaisEntrada = Convert.ToChar(cb_Pais_Ll.SelectedValue.ToString());
+                Obj_Editar_Destino.cIdEstado = Convert.ToChar(cb_Estado.SelectedValue.ToString());
+
+                if (Obj_Editar_Destino.cAccion == 'I')
+                {
+                    Obj_Destinos_BLL.InsertarDestinos(ref Obj_Editar_Destino);
+
+                    if (Obj_Editar_Destino.sMsjError == string.Empty)
+                    {
+                        MessageBox.Show("Se guardo exitosamente el nuevo Destino", "EXITO",
+                          MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Se presento un error a la hora de guardar el nuevo Destino. \n \n Error [ " + Obj_Editar_Destino.sMsjError + " ].", "ERROR",
+                           MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    Obj_Destinos_BLL.ModificarDestinos(ref Obj_Editar_Destino);
+
+                    if (Obj_Editar_Destino.sMsjError == string.Empty)
+                    {
+
+                        MessageBox.Show("Se modifico exitosamente los Datos De Destino", "EXITO",
+                           MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Se presento un error a la hora de modificar los Datos De Destino. \n \n Error [ " + Obj_Editar_Destino.sMsjError + " ].", "ERROR",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                CargarDatos();
+            }
+        }
+
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FRM_Editar_Destinos_Load(object sender, EventArgs e)
+        {
+            CargarDatos();
+        }
+
+        private void txt_ID_Destino_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+            if (char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+        }
+
+        private void txt_Nombre_Destino_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+            if (char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
         }
     }
 }
